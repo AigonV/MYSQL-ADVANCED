c3c8a6cf23d42988a5dba1e6d626c08cc60ec268
Mohammed Ahmed <2149869@talnet.nl>
2022-02-25 17:32:34 +0100

    Opdracht 1: MySQL-Advanced

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,11 @@
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (1, "Mohammed", "Ahmed", "9D", "Van der Hooplaan 809", "1182TP", "2003-10-21", "Zuid-Oost")
 
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (5, "Thomas", "Venhorst", "1C", "Van Heuven Goedhartlaan 809", "1181LB", "2003-09-20", "Amstelveen")
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (6, "Marlon", "Stapler", "8B", "Renegade 862", "1998IO", "2001-12-22", "Amsterdam")
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (7, "Abdel", "Habibi", "9E", "Amsterdamseweg 465", "1182TP", "2004-06-03", "Amstelveen")
