d3039ab586a3a02b3bb04c57d215a2488d85f639
Mohammed Ahmed <2149869@talnet.nl>
2022-02-28 11:36:19 +0100

    Opdracht 2: MySQL Level 2

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,8 @@
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, geboortedatum, woonplaats)
+VALUES (8, "Jameel", "Baboon", "9C", "1998-10-21", "Zuid-Oost")
 
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, geboortedatum, woonplaats)
+VALUES (9, "Jamal", "Ape", "9C", "1999-07-12", "Uithoorn")
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, geboortedatum, woonplaats)
+VALUES (10, "Tyrone", "Monkey", "9C", "2002-11-21", "Uithoorn")
